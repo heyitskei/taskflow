eb3f2b363604b20011c591106d271de5ec8ed6f6
e.stroganova <e.stroganova@vehikl.com>
2024-11-12 16:40:00 -0500

    initialize + setup inertia

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        Hi
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    content: ['./resources/**/*.blade.php', './resources/**/*.{js,vue}'],
+    theme: {extend: {},},
+    plugins: [],
+};
