40c3daef791d34189bc4a3b07c2ff9881d7bd292
e.stroganova <e.stroganova@vehikl.com>
2024-11-18 17:01:40 -0500

    add containers
    wip:
    - bottom AI container needs more love

--- resources/js/Pages/Test.vue
+++ resources/js/Pages/Test.vue
@@ -3,8 +3,96 @@
 </script>
 
 <template>
-    <div>
-        Hi
+    <div class="flex flex-col">
+        <div>
+            <p class="text-center mb-10 text-3xl">TaskFlow</p>
+        </div>
+        <div class="grid grid-cols-12 gap-4">
+            <div class="text-center col-span-3 bg-gray-100">
+                <p class="text-2xl">Daily News Digest</p>
+                <div class="bg-gray-200 h-96">Hi</div>
+            </div>
+            <div class="col-span-6 bg-red-100">
+                <p class="text-center text-2xl">Calendar</p>
+                <table class="bg-gray-200 h-96 w-full">
+                    <thead>
+                    <tr class="text-center ml-4 mr-4">
+                        <th>1</th>
+                        <th>2</th>
+                        <th>3</th>
+                        <th>4</th>
+                        <th>5</th>
+                        <th>6</th>
+                        <th>7</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr class="text-center ml-4 mr-4">
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>5</td>
+                        <td>6</td>
+                        <td>7</td>
+                    </tr>
+                    <tr class="text-center ml-4 mr-4">
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>5</td>
+                        <td>6</td>
+                        <td>7</td>
+                    </tr>
+                    <tr class="text-center ml-4 mr-4">
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>5</td>
+                        <td>6</td>
+                        <td>7</td>
+                    </tr>
+                    <tr class="text-center ml-4 mr-4">
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>5</td>
+                        <td>6</td>
+                        <td>7</td>
+                    </tr>
+                    <tr class="text-center ml-4 mr-4">
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>5</td>
+                        <td>6</td>
+                        <td>7</td>
+                    </tr>
+                    <tr class="text-center ml-4 mr-4">
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>5</td>
+                        <td>6</td>
+                        <td>7</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="text-center col-span-3 bg-green-100">
+                <p class="text-2xl">Daily Detailed View</p>
+                <div class="bg-gray-200 h-96">Hi</div>
+            </div>
+        </div>
+        <div class="container mx-auto text-center h-40 bg-orange-100">
+            <input class="align-bottom" placeholder="Here is your AI" type="text"/>
+            <button class="align-bottom" type="submit">Enter</button>
+        </div>
     </div>
 </template>
 
