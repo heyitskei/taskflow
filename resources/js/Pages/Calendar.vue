2c220ba9da468b81ca1abee8225cda9bb1291a07
e.stroganova <e.stroganova@vehikl.com>
2024-12-10 17:01:32 -0500

    calendar work

--- resources/js/Pages/Calendar.vue
+++ resources/js/Pages/Calendar.vue
@@ -1,32 +1,13 @@
 <template>
     <div class="col-span-6 bg-red-100">
-        <VCalendar/>
+        <VCalendar v-model="value" :events="events" @click:date="(e) => console.log(e)"/>
     </div>
-    <!--        <h2>{{currentMonth}}</h2>-->
-    <!--        <table class="bg-gray-200 h-96 w-full">-->
-    <!--            <thead>-->
-    <!--            <tr class="text-center ml-4 mr-4">-->
-    <!--                <th>1</th>-->
-    <!--                <th>2</th>-->
-    <!--                <th>3</th>-->
-    <!--                <th>4</th>-->
-    <!--                <th>5</th>-->
-    <!--                <th>6</th>-->
-    <!--                <th>7</th>-->
-    <!--            </tr>-->
-    <!--            </thead>-->
-    <!--            <tbody>-->
-    <!--            <tr v-for="day in daysInMonth" class="text-center ml-4 mr-4">-->
-    <!--&lt;!&ndash;                <td>{{ day }}</td>&ndash;&gt;-->
-    <!--            </tr>-->
-    <!--            </tbody>-->
-    <!--        </table>-->
-    <!--    </div>-->
 </template>
 
 
 <script setup>
 import {VCalendar} from 'vuetify/labs/VCalendar'
+import {computed} from 'vue'
 
 const props = defineProps({
     currentMonth: {
@@ -39,16 +20,14 @@ const props = defineProps({
     },
 });
 
-//7 slots
-//days of month are
+const value = new Date();
 
-//division - double vfor? put the numbers in the slots
+const events = computed(() => [{
+    title: 'my event',
+    start: new Date(),
+    end: new Date(),
+    color: 'red',
+    allDay: true,
+}])
 
-
-// const rows = [
-//     {
-//         id: 1,
-//         data: 'hi'
-//     }
-// ]
 </script>
