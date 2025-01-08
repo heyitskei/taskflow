87f08e83572130dec3c5900656025714a94a81b7
e.stroganova <e.stroganova@vehikl.com>
2025-01-08 09:31:16 -0500

    more calendar work

--- resources/js/Pages/Calendar.vue
+++ resources/js/Pages/Calendar.vue
@@ -1,33 +1,35 @@
 <template>
     <div class="col-span-6 bg-red-100">
-        <VCalendar v-model="value" :events="events" @click:date="(e) => console.log(e)"/>
+        <VCalendar
+            v-model="value"
+            :events="events"
+            @click:date="handleDateClick"
+        />
+        <button @click="addEvent">Add Event</button>
     </div>
 </template>
 
-
 <script setup>
 import {VCalendar} from 'vuetify/labs/VCalendar'
-import {computed} from 'vue'
+import {reactive, ref} from 'vue'
 
-const props = defineProps({
-    currentMonth: {
-        type: String,
-        required: true,
-    },
-    daysInMonth: {
-        type: Number,
-        required: true,
-    },
-});
+const value = ref(new Date());
 
-const value = new Date();
+const events = reactive([]);
 
-const events = computed(() => [{
-    title: 'my event',
-    start: new Date(),
-    end: new Date(),
-    color: 'red',
-    allDay: true,
-}])
+function handleDateClick(event) {
+    console.log('Clicked date:', event);
+}
 
+function addEvent() {
+    const newEvent = {
+        title: `New Event ${events.length + 1}`,
+        start: new Date(),
+        end: new Date(),
+        color: 'blue',
+        allDay: true,
+    };
+    events.push(newEvent);
+}
 </script>
+
